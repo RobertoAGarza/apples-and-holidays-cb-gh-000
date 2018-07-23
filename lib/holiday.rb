7251f04d497ab1c3a009702bb2e466859fd5a589
Roberto Garza <robertg4567@hotmail.com>
2018-07-23 17:03:49 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,9 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holidays|
     puts "#{season.to_s.capitalize}:"
     holidays.each do |attribute, value|
-      puts "   #{attribute}: #{value}"
+      holiday = attribute.split(" ")
+      holiday.each do |item|
+        puts "  #{item.capitalize}"
     end
   end
 end
