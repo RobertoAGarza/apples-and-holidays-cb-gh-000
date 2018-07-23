9a813418bc6bc5f14ce2be815dc658748ecf20cd
Roberto Garza <robertg4567@hotmail.com>
2018-07-23 17:09:12 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,8 +60,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holidays|
     puts "#{season.to_s.capitalize}:"
     holidays.each do |attribute, value|
-
-      puts "  #{attribute.capitalize}: #{attribute.values.flatten}"
+      puts "  #{attribute.capitalize}: #{value}"
     end
   end
 end
