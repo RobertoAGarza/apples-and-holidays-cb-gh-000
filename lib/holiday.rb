38720bb0b6da83937968a89705f09706eca88889
Roberto Garza <robertg4567@hotmail.com>
2018-07-23 17:33:14 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,7 +67,7 @@ def all_supplies_in_holidays(holiday_hash)
       end
       array.join(" ")
       myString = "  #{array[0]}: "
-      myString = myString +  value.map { |i| "'" + i.to_s + "'" }.join(",")
+      myString = myString +  "#{value.map { |i| "'" + i.to_s + "'" }.join(",")}"
     end
   end
 end
