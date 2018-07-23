ad7510e26ba5de5fea12e467f00ba819ebd31552
Roberto Garza <robertg4567@hotmail.com>
2018-07-23 17:24:15 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,7 +66,7 @@ def all_supplies_in_holidays(holiday_hash)
         word = word.capitalize!
       end
       array.join(" ")
-      puts "#{array.values}: #{value}"
+      puts "  #{array[0]}: #{value}"
     end
   end
 end
